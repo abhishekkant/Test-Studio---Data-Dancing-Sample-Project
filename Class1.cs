385daecd58ac8e42cc50b03243f646c9b2ae469f
ProgressTestStudio <progressteststudio@gmail.com>
2021-03-24 09:24:50 -0500

    adding more tests

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,28 @@ using System.Linq;
 namespace Data_Dancing
 {
     public class Class1
+    
     {
+        /// <summary>
+        /// Create a random number
+        /// </summary>
+        /// <param name="min">minimum number</param>
+        /// <param name="max">maximum number</param>
+        /// <returns>number between min and max</returns>
+        public static int RandomNumberGenerator(int min, int max) {
+            
+            Random rnd = new Random();
+            
+            int randomProduct = rnd.Next(min, max);
+            
+            return randomProduct;
+            
+            
+        }
+        
         
     }
+
+
+    
 }
